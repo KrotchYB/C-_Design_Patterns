41ba51c25bbbca5707e6cd0518464496222ce9ed
Burhan <brh.rdn@gmail.com>
2023-05-08 23:59:24 +0300

    -Singleton Pattern C#

---
+++ Singleton_Pattern/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+//no thread safe
+public sealed class Calculator
+{
+    #region Member(s)
+    private static Calculator instance;
+    public static Calculator Instance 
+    { 
+        get 
+        { 
+            if(instance == null) 
+            {
+                instance = new Calculator();
+            }
+            return instance;
+        } 
+    }
+    #endregion
+
+    #region Method(s)
+    public void Addition(int x, int y) 
+    {
+        Console.WriteLine("x + y is equal to : " +( x + y));
+    }
+
+    public void Substraction(int x, int y)
+    {
+        Console.WriteLine("x - y is equal to : "+ (x - y));
+    }
+
+    public void Multiplication(int x, int y)
+    {
+        Console.WriteLine("x * y is equal to : " + (x * y));
+    }
+
+    public void Division(int x, int y)
+    {
+        Console.WriteLine("x / y is equal to : " + (x / y));
+    }
+    #endregion
+
+}
---
+++ Singleton_Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+static class Program
+{
+    static void Main()
+    {
+        try
+        {   
+            Calculator.Instance.Addition(10, 2);
+            Calculator.Instance.Substraction(10, 2);
+            Calculator.Instance.Multiplication(10, 2);
+            Calculator.Instance.Division(10, 2);
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex.Message);
+        }
+    }
+}
